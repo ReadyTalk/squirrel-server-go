63ad66571b76b1a95deed0f65015ed05f66000e9
Joshua Warner <joshua.warner@readytalk.com>
2015-02-13 15:12:23 -0700

    switch to caching pseudo-proxy

--- server.go
+++ server.go
@@ -1,47 +1,144 @@
 package main
 
 import (
-    "io/ioutil"
+    // "io/ioutil"
     "log"
-    "os"
+    // "os"
     "flag"
-    "strings"
+    "time"
+    "regexp"
+    "encoding/json"
+    "sync"
     "net/http"
     "net/url"
-    "path/filepath"
+    "io/ioutil"
+    // "path/filepath"
 )
+
+type VersionInfo struct {
+    LastUpdateCheckTime time.Time
+    StatusCode int
+    VersionNumber string
+    Data []byte
+}
+
+type VersionCache struct {
+    cache map [string] *VersionInfo
+}
+
+func NewVersionCache() *VersionCache {
+    return &VersionCache{make(map [string] *VersionInfo)}
+}
+
+func (vc *VersionCache) GetOrLookup(url string) VersionInfo {
+    cached := vc.cache[url]
+    if cached == nil || time.Since(cached.LastUpdateCheckTime) > 30 * time.Second {
+        resp, err := http.Get(url)
+
+        if err != nil {
+            log.Print(err)
+            return VersionInfo {
+                time.Now(),
+                500,
+                "<unknown>",
+                nil,
+            }
+        }
+
+        data, err := ioutil.ReadAll(resp.Body)
+
+        if err != nil {
+            log.Print(err)
+            return VersionInfo {
+                time.Now(),
+                500,
+                "<unknown>",
+                nil,
+            }
+        }
+
+        log.Printf("Got %d from %s", resp.StatusCode, url)
+
+        if resp.StatusCode != 200 {
+            return VersionInfo {
+                time.Now(),
+                resp.StatusCode,
+                "<unknown>",
+                data,
+            }
+        }
+
+        var decoded map[string]interface{}
+
+        if err := json.Unmarshal(data, &decoded); err != nil {
+            log.Print("Couldn't decode ", url, " : ", err)
+            return VersionInfo {
+                time.Now(),
+                resp.StatusCode,
+                "<unknown>",
+                data,
+            }
+        }
+
+        versionInfo := VersionInfo {
+            time.Now(),
+            resp.StatusCode,
+            decoded["version"].(string),
+            data,
+        }
+
+        cached = &versionInfo
+        vc.cache[url] = cached
+    }
+
+    return *cached
+}
 
 func main() {
     address := flag.String("address", ":3000", "Address to listen on")
-    dir := flag.String("dir", "releases", "Directory to serve version data from")
+    urlregexp_str := flag.String("regexp", ".*", "Regular expressoin for valid urls to proxy")
     flag.Parse()
 
-    filepath.Walk(*dir, func(path string, f os.FileInfo, err error) error {
-        if err != nil {
-            panic(err)
+    urlregexp, err := regexp.Compile(*urlregexp_str);
+    if err != nil {
+        panic(err);
+    }
+
+    cache := NewVersionCache()
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        query, _ := url.ParseQuery(r.URL.RawQuery)
+        queryVersions := query["version"]
+        dest := query["url"]
+
+        if len(dest) != 1 || len(queryVersions) > 1 {
+            w.WriteHeader(http.StatusBadRequest)
+            return
         }
 
-        if filepath.Ext(path) == ".json" {
-            servePath := filepath.ToSlash(strings.TrimPrefix(filepath.Dir(path), *dir))
-            if !strings.HasPrefix(servePath, "/") {
-                servePath = "/" + servePath
+        valid := urlregexp.MatchString(dest[0])
+
+        if !valid {
+            w.WriteHeader(http.StatusForbidden)
+            return
+        }
+
+        versionInfo := cache.GetOrLookup(dest[0])
+
+        if versionInfo.StatusCode != 200 {
+            w.WriteHeader(versionInfo.StatusCode)
+            if versionInfo.Data != nil {
+                w.Write(versionInfo.Data)
             }
-            version := strings.TrimSuffix(filepath.Base(path), ".json")
-            data, _ := ioutil.ReadFile(path)
-            log.Printf("serving %s version %s", servePath, version)
-            http.HandleFunc(servePath, func(w http.ResponseWriter, r *http.Request) {
-                query, _ := url.ParseQuery(r.URL.RawQuery)
-                queryVersions := query["version"]
-                if len(queryVersions) > 0 && queryVersions[0] == version {
-                    w.WriteHeader(204)
-                } else {
-                    w.Header().Add("Content-Type", "application/json")
-                    w.Write(data)
-                }
-            })
+            return
         }
 
-        return nil
+        if len(queryVersions) == 1 && versionInfo.VersionNumber == queryVersions[0] {
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
+
+        w.Write(versionInfo.Data)
     })
 
     log.Fatal(http.ListenAndServe(*address, nil))
